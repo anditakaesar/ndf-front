6ea62afc9157f9ecc8ff803dabfd16d685fd74a9
anditakaesar <anditakaesar@gmail.com>
2019-07-02 10:42:13 +0700

    first working component

--- src/components/QrCodeReader/QrCodeReader.jsx
+++ src/components/QrCodeReader/QrCodeReader.jsx
@@ -1,13 +1,61 @@
 import React from 'react';
+import QrReader from 'react-qr-reader'
+import './QrCodeReader.css';
 
 class QrCodeReader extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            result: 'No result',
+            isFound: false,
+        }
+    }
 
-    handleOpenWindow = () => {
-        console.log(`Clicked`);
+    handleScan = data => {
+        if (data) {
+            this.setState({
+                result: data,
+                isFound: true
+            });
+        }
+    }
+
+    returnStyle() {
+        if (this.state.isFound) {
+            return {
+                'display': 'none'
+            }
+        } else {
+            return {
+                'display': 'inline'
+            }
+        }
+    }
+
+    handleError = err => {
+        console.error(err);
+    }
+
+    handleBeginScan = () => {
+        this.setState({
+            isFound: false
+        });
     }
 
     render() {
-        return <button onClick={this.handleOpenWindow}>Open Window</button>
+        return (
+            <React.Fragment>
+                <div className="reader-container">
+                    <QrReader 
+                        ref="test"
+                        delay={500} 
+                        onError={this.handleError} 
+                        onScan={this.handleScan} />
+                </div>
+                <p>{this.state.result}</p>
+                <button onClick={this.handleBeginScan}>Begin Scan</button>
+            </React.Fragment>
+        );
     }
 }
 
